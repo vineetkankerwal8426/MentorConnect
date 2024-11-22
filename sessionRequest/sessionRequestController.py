aa9789a2c28abe460290489c2ca88e6c006753b9
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2024-11-23 00:29:22 +0530

    rest api for sesssion request features commited

--- sessionRequest/sessionRequestController.py
+++ sessionRequest/sessionRequestController.py
@@ -1,13 +1,32 @@
-from flask import request
+from flask import request,session
 from app import app
 from sessionRequest.sessionRequestLogic import sessionRequestLogic
 
 
 
 @app.route("/sessionaccepted",methods = ["POST"])
-def sessionRequestControllerFunc():
+def sessionRequestAcceptedControllerFunc():
     sessionRequestLogicObj = sessionRequestLogic()
     data = request.json
     reqid = data.get("sessionrequestid")
     sessionlink = data.get('sessionlink')
-    return sessionRequestLogicObj.sessionRequestAcceptFuction(reqid,sessionlink)
+    return sessionRequestLogicObj.sessionRequestAcceptFunction(reqid,sessionlink)
+
+@app.route("/sessionrejected",methods=["DELETE"])
+def sessionRequestRejectedControllerFunc():
+    sessionRequestLogicObj = sessionRequestLogic()
+    data = request.json
+    reqid = data.get("sessionrequestid")
+    return sessionRequestLogicObj.sessionRequestRejectFunction(reqid)
+
+@app.route("/requestsession",methods=["POST"])
+def requestSessionControllerFunction():
+    sessionRequestLogicObj = sessionRequestLogic()
+    data = request.json
+    mentorIdname = data.get("mentorUserIdName")
+    # print(mentorIdname)
+    sessionDate = data.get("sessionDate")
+    sessionTime = data.get("sessionTime")
+    sessionTopic = data.get("sessionTopic")
+    sessionMessage = data.get("sessionMessage")
+    return sessionRequestLogicObj.requestSessionFunction(mentorIdname,sessionDate,sessionTime,sessionTopic,sessionMessage)
--- sessionRequest/sessionRequestLogic.py
+++ sessionRequest/sessionRequestLogic.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import jsonify
+from flask import jsonify,session
 
 class sessionRequestLogic():
     def __init__(self):
@@ -9,9 +9,46 @@ class sessionRequestLogic():
         except:
             return jsonify({'status': 'fail', 'message': 'database connection failed'}), 401
         
-    def sessionRequestAcceptFuction(self,reqid,sessionlink):
-        query1 = f"SELECT * from sessionrequests where reqid = '{reqid}'"
-        self.cur.execute(query1)
-        sessionrequest_info = self.cur.fetchone()
-        print(sessionrequest_info)
-        return "succes"
+    def sessionRequestAcceptFunction(self,reqid,sessionlink):
+        try:
+            query1 = f"SELECT * from sessionrequests where reqid = '{reqid}'"
+            self.cur.execute(query1)
+            sessionrequest_info = self.cur.fetchone()
+
+            query2 = "insert into sessions (sessionTopic,sessionDate,sessionTime,sessionLink,userId,mentorId,sessionMessage) values (%s,%s,%s,%s,%s,%s,%s)"
+            self.cur.execute(query2, (sessionrequest_info['sessionTopic'],sessionrequest_info['sessionDate'],sessionrequest_info['sessionTime'],sessionlink,sessionrequest_info['userId'],sessionrequest_info['mentorId'],sessionrequest_info['sessionMessage']))
+            self.con.commit()
+
+            query3 = f"delete from sessionrequests where reqid = '{reqid}'"
+            self.cur.execute(query3)
+            self.con.commit()
+
+            return jsonify({'status': 'success'})
+            # print(sessionrequest_info)
+            # return "hello"
+        except:
+            return jsonify({'status': 'fail'}), 401
+        
+    def sessionRequestRejectFunction(self,reqid):
+        query = f"delete from sessionrequests where reqid = {reqid}"
+        try:
+            self.cur.execute(query)
+            self.con.commit()
+            return jsonify({'status': 'success'})
+        except:
+            return jsonify({'status': 'fail'}), 401
+        
+    def requestSessionFunction(self,mentorIdname,sessionDate,sessionTime,sessionTopic,sessionMessage):
+        sessionUserInfo = session.get("userInfo")
+        try:
+            query1 = f"select userid from users where useridname = '{mentorIdname}'"
+            print(mentorIdname)
+            self.cur.execute(query1)
+            mentorId = self.cur.fetchone()
+            
+            query2 = "insert into sessionrequests (userId,mentorId,sessionDate,sessionTime,sessionTopic,sessionMessage) values (%s,%s,%s,%s,%s,%s)"
+            self.cur.execute(query2,(sessionUserInfo['userId'],mentorId["userid"],sessionDate,sessionTime,sessionTopic,sessionMessage))
+            self.con.commit()
+            return jsonify({'status': 'success'})
+        except:
+            return jsonify({'status': 'fail'}), 401
